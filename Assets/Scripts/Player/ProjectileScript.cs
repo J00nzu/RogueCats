7a7b2bbb1b4cfbf0f47d84926ebd59563947f907
Ninya <sleepyneko@protonmail.ch>
2018-05-06 19:04:29 +0300

    Basic player control and shooting

---
+++ Assets/Scripts/Player/ProjectileScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+	private float speed = 5f;
+	private float damage = 1f;
+	private bool bouncy = false;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DeSpawn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.up * speed * Time.deltaTime;
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (!bouncy) {
+			if (gameObject != null) {
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	IEnumerator DeSpawn(){
+		yield return new WaitForSeconds(5);
+		if (gameObject != null) {
+			Destroy(gameObject);
+		}
+	}
+
+	public void SetStats(float speed, float damage, bool bouncy){
+		this.speed = speed;
+		this.damage = damage;
+		this.bouncy = bouncy;
+	}
+
+}
---
+++ Assets/Scripts/Player/StaffScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffScript : MonoBehaviour {
+	
+	GameObject bullet;
+	private bool canShoot = true;
+
+	private float speed = 5f;
+	private float damage = 1f;
+	private bool bouncy = false;
+	private float shotRate = 1f;
+
+	// Use this for initialization
+	void Start () {
+		bullet = Resources.Load("Prefabs/Bullet") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Shoot(){
+		if (canShoot) {
+			GameObject projectile = Instantiate(bullet, transform.position + transform.up, transform.rotation);
+			projectile.GetComponent<ProjectileScript>().SetStats(speed, damage, bouncy);
+
+			canShoot = false;
+			StartCoroutine(CoolDown());
+		}
+	}
+
+	IEnumerator CoolDown(){
+		yield return new WaitForSeconds(1 / shotRate);
+		canShoot = true;
+	}
+
+	public void SetStats(float speed, float damage, bool bouncy) {
+		this.speed = speed;
+		this.damage = damage;
+		this.bouncy = bouncy;
+	}
+}
