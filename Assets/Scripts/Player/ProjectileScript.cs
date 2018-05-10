a24538c3d905170950a9a2e48f5f9abebff20ffd
Ninya <sleepyneko@protonmail.ch>
2018-05-10 15:39:37 +0300

    Player and projectile stuffen

--- Assets/Scripts/Player/ProjectileScript.cs
+++ Assets/Scripts/Player/ProjectileScript.cs
@@ -7,22 +7,31 @@ public class ProjectileScript : MonoBehaviour {
 	private float speed = 5f;
 	private float damage = 1f;
 	private bool bouncy = false;
+	private int bounceTimes = 0;
+	Rigidbody2D rb2;
 
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(DeSpawn());
+		rb2 = GetComponent<Rigidbody2D>();
+		rb2.velocity = transform.up * speed;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.position += transform.up * speed * Time.deltaTime;
+	void FixedUpdate () {
+		//rb2.velocity = rb2.velocity.normalized * speed;
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
 		if (!bouncy) {
 			if (gameObject != null) {
 				Destroy(gameObject);
+			}
+		}else if(bounceTimes > 0){
+			bounceTimes--;
+			if(bounceTimes < 1){
+				bouncy = false;
 			}
 		}
 	}
@@ -34,10 +43,11 @@ public class ProjectileScript : MonoBehaviour {
 		}
 	}
 
-	public void SetStats(float speed, float damage, bool bouncy){
+	public void SetStats(float speed, float damage, bool bouncy, int bounceTimes){
 		this.speed = speed;
 		this.damage = damage;
 		this.bouncy = bouncy;
+		this.bounceTimes = bounceTimes;
 	}
 
 }
