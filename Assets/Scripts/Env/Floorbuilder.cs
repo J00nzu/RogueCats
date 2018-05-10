b720be284ad7fa79bbd888723d7d8f5e7d96f53f
Nom <jonilakkala@hotmail.com>
2018-05-10 14:15:31 +0300

    Some tile testing

--- Assets/Scripts/Env/Floorbuilder.cs
+++ Assets/Scripts/Env/Floorbuilder.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomBuilder : MonoBehaviour {
+public class Floorbuilder : MonoBehaviour {
+
+
 
 	// Use this for initialization
 	void Start () {
